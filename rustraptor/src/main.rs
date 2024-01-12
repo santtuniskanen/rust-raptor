ed879fafbbc70f69add80c8cbc0f3feceebdedd5
santtuniskanen <santtuniska02@gmail.com>
2024-01-12 12:33:41 +0200

    finished tutorial

--- rustraptor/src/main.rs
+++ rustraptor/src/main.rs
@@ -11,7 +11,7 @@ fn main() {
     // Passes the vector `args` to the parse_config function
     //
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
@@ -19,7 +19,7 @@ fn main() {
     println!("In file {}", config.file_path);
 
     if let Err(e) = rustraptor::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
