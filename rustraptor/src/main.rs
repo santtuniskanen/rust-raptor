6ada44d13af23bb447adcac69995105babe8e02a
santtuniskanen <santtuniska02@gmail.com>
2024-01-12 11:51:09 +0200

    Refactored rustraptor into main and lib

--- rustraptor/src/main.rs
+++ rustraptor/src/main.rs
@@ -1,4 +1,5 @@
-use std::{env, fs};
+use std::{env, process};
+use rustraptor::Config;
 
 fn main() {
     //
@@ -9,23 +10,16 @@ fn main() {
     //
     // Passes the vector `args` to the parse_config function
     //
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err|{
+        println!("Problem parsin arguments: {err}");
+        process::exit(1);
+        });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
     
-    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-    println!("With text:\n{contents}");
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Config{ query, file_path }
+    if let Err(e) = rustraptor::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
