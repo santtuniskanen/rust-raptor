87e4f0ba011b86bbfe170c8a1f05be78be598a4b
santtuniskanen <santtuniska02@gmail.com>
2024-01-19 23:26:21 +0200

    added some comments and file validation

--- rustraptor/src/file_ops.rs
+++ rustraptor/src/file_ops.rs
@@ -1,6 +1,25 @@
+use std::path::Path;
 use std::fs;
 use std::error::Error;
 
 pub fn read_file_contents(file_path: &str) -> Result<String, Box<dyn Error>> {
+    //
+    // Function first validates user input. If validation passes,
+    // the function will read the contents in a String format.
+    //
+    if !is_valid_file_path(file_path) {
+        return Err("Invalid file path".into());
+    }
+
     fs::read_to_string(file_path).map_err(|e| e.into())
+}
+
+fn is_valid_file_path(file_path: &str) -> bool {
+    //
+    // Function checks if a given input item on the file_path is a file using `is_file()` function,
+    // which returns a boolean. The function also makes sure that the given item or path
+    // exists by using the `exists()` function, returning a boolean.
+    //
+    let path = Path::new(file_path);
+    path.is_file() && path.exists()
 }
--- rustraptor/src/main.rs
+++ rustraptor/src/main.rs
@@ -1,7 +1,14 @@
 use rustraptor::{Config, run};
 
 fn main() {
+    //
+    // Collects the command line arguments into a vector
+    //
     let args: Vec<String> = std::env::args().collect();
+
+    //
+    // Passes the vector `args` to the parse_config function
+    //
     let config = Config::build(&args).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         std::process::exit(1);
