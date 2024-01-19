1f49eb98f5436427fb479fe79e1ba9f5477013a4
santtuniskanen <santtuniska02@gmail.com>
2024-01-19 23:51:25 +0200

    separated tests into their own directory

--- rustraptor/src/lib.rs
+++ rustraptor/src/lib.rs
@@ -25,34 +25,3 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn one_result() {
-        let query = "duct";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.";
-
-        assert_eq!(vec!["safe, fast, productive."], search::search(query, contents));
-    }
-
-    #[test]
-    fn case_insensitive() {
-        let query = "rUsT";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Trust me.";
-
-        assert_eq!(
-            vec!["Rust:", "Trust me."],
-            search::search_case_insensitive(query, contents)
-        );
-    }
-}
